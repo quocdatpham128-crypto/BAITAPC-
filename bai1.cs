29b7b0148bdf221998823d6e29b8d993424bbf49
datpham1705 <quocdatpham128@gmai.com>
2026-03-17 22:38:38 +0700

    add code

---
+++ bai1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+
+class bai1
+{
+    static void GT()
+    {
+        Console.WriteLine("Pham Quoc Dat");
+        Console.WriteLine("2415053122205");
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("\n\n");
+    }
+    static void Main(string[] args)
+    {
+        GT();
+        List<int> ds = new List<int>();
+        Console.WriteLine("Nhap day so :");
+        string[] ch = Console.ReadLine().Split(' ');
+        foreach (string s in ch)
+        {
+            ds.Add(int.Parse(s));
+        }
+        Console.WriteLine("Nhap 5 so can them:");
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Nhap so thu " + (i + 1) + ": ");
+            int so = int.Parse(Console.ReadLine());
+            ds.Add(so);
+        }
+        Console.WriteLine("Danh sach sau khi them:");
+
+        foreach (int i in ds)
+        {
+            Console.Write(i + " ");
+        }
+    }
+}
