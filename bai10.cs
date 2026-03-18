69ce93342bdf04a97d3e2d37bc4c6cda5ff306a5
datpham1705 <quocdatpham128@gmail.com>
2026-03-18 23:14:02 +0700

    add code

---
+++ bai10.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+class bai10
+    {
+        static void GT()
+        {
+            Console.WriteLine("Pham Quoc Dat");
+            Console.WriteLine("2415053122205");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("\n\n");
+        }
+        static void Main(string[] args)
+        {
+            GT();
+            Console.WriteLine("nhap danh sach so nguyen");
+            String[] chuoi = Console.ReadLine().Split(' ');
+            int min=int.MaxValue,tg;
+            foreach (string s in chuoi)
+            {
+                tg = int.Parse(s);
+                if (min > tg) min = tg;
+            }
+        Console.WriteLine($"gia tri nho nhat trong danh sach la: {min}");
+        }
+    }
+
