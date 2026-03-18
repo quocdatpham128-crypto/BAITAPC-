d636f5f3dab03f48d792d16d03e924e4e1b0bf93
datpham1705 <quocdatpham128@gmail.com>
2026-03-18 23:15:44 +0700

    đẩy code

---
+++ bai13.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+class student
+{
+    public int Id;
+    public string Name;
+
+}
+class bai13
+{
+    static void GT()
+    {
+        Console.WriteLine("Pham Quoc Dat");
+        Console.WriteLine("2415053122205");
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("\n\n");
+    }
+    static void Main(string[] args)
+    {
+        GT();
+        List<student> ds = new List<student>();
+        Console.WriteLine("Nhap danh sach sinh vien");
+        String[] ch = Console.ReadLine().Split(' ');
+        Console.WriteLine($"so luong sinh vien {ch.Length/2}");
+    }
+}
