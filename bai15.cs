eebc21323010ade6cf88180645c850a81a3e2b56
datpham1705 <quocdatpham128@gmail.com>
2026-03-18 23:16:56 +0700

    addcode

---
+++ bai15.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+class bai15
+{
+    static void GT()
+    {
+        Console.WriteLine("Pham Quoc Dat");
+        Console.WriteLine("2415053122205");
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("\n\n");
+    }
+    static void Main(string[] args)
+    {
+        GT();
+        Dictionary<string, string> ds = new Dictionary<string, string>();
+        Console.WriteLine("Nhap cac tu de tao tu dien");
+        String[] ch = Console.ReadLine().Split();
+        for ( int i= 0; i< ch.Length; i+= 2)
+          ds.Add(ch[i], ch[i + 1]);
+        Console.WriteLine("Nap tu de tra nghia");
+        string t = Console.ReadLine();
+        Console.WriteLine($"Nghia cua tu {t} la {ds[t]}");
+           
+        }
+    }
+
