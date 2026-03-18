d53e36c3f3d1153fc471b5c90ccc4ad3accff1f8
datpham1705 <quocdatpham128@gmail.com>
2026-03-18 23:13:39 +0700

    add  code

---
+++ bai9.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class bai9
+{
+        static void GT()
+        {
+            Console.WriteLine("Pham Quoc Dat");
+            Console.WriteLine("2415053122205");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("\n\n");
+        }
+        static void Main(string[] args)
+        {
+            GT();
+            List<int> ds = new List<int>();
+        List<int> kq = new List<int>();
+
+        Console.WriteLine("Nhap day so:");
+        string[] arr = Console.ReadLine().Split(' ');
+
+        foreach (string s in arr)
+        {
+            ds.Add(int.Parse(s));
+        }
+
+        foreach (int i in ds)
+        {
+            if (!kq.Contains(i))
+            {
+                kq.Add(i);
+            }
+        }
+
+        Console.WriteLine("Danh sach khong trung:");
+
+        foreach (int i in kq)
+        {
+            Console.Write(i + " ");
+        }
+    }
+}
