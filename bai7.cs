b1bd4b2b9a71d3d94ddb2a27bc1ccc4f5a823298
datpham1705 <quocdatpham128@gmail.com>
2026-03-18 23:12:26 +0700

    add code

---
+++ bai7.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class bai7
+{
+    static void GT()
+    {
+        Console.WriteLine("Pham Quoc Dat");
+        Console.WriteLine("2415053122205");
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("\n\n");
+    }
+    static void Main(string[] args)
+    {
+        GT();
+        List<int> ds = new List<int>();
+
+        Console.WriteLine("Nhap day so:");
+        string[] arr = Console.ReadLine().Split(' ');
+
+        foreach (string s in arr)
+        {
+            ds.Add(int.Parse(s));
+        }
+
+        ds.Sort();
+
+        Console.WriteLine("Danh sach sau khi sap xep:");
+
+        foreach (int i in ds)
+        {
+            Console.Write(i + " ");
+        }
+    }
+}
