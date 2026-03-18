0e5cc316615d76cbc3e5e16273a829cb99c7d3af
datpham1705 <quocdatpham128@gmail.com>
2026-03-18 23:16:18 +0700

    add code

---
+++ bai14.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+class student
+{
+    public int Id;
+    public string Name;
+
+}
+class bai14
+{
+    static void GT()
+    {
+        Console.WriteLine("Pham Quoc Dat");
+        Console.WriteLine("2415053122205");
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("\n\n");
+    }
+    static void Main(string[] args)
+    {
+        GT();
+        List<student> ds = new List<student>();
+        Console.WriteLine("Nhap danh sach sinh vien");
+        String[] ch = Console.ReadLine().Split(' ');
+        for (int i = 0; i < ch.Length; i += 2)
+        {
+            student sv = new student();
+            sv.Id = int.Parse(ch[i]);
+            sv.Name = ch[i + 1];
+            ds.Add(sv);
+        }
+        Console.WriteLine("Nhap Id sinh vien can xoa");
+        int id = int.Parse(Console.ReadLine());
+        Console.WriteLine("Danh sach sinh vien sau khi xoa");
+        ds.RemoveAll(sv=>sv.Id == id);
+        foreach (student sv in ds)
+        { 
+              Console.WriteLine($"Id {sv.Id} - Ten {sv.Name}");
+        }
+    }
+}
