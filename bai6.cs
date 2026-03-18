21200b22e1e05009b5d21ae28f5a417a6e11125b
datpham1705 <quocdatpham128@gmail.com>
2026-03-18 23:11:58 +0700

    addcode

---
+++ bai6.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class bai6
+{
+    static void GT()
+    {
+        Console.WriteLine("Pham Quoc Dat");
+        Console.WriteLine("2415053122205");
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("\n\n");
+    }
+    static void Main(string[] args)
+    {
+        GT();
+        List<string> ds = new List<string>();
+
+        Console.WriteLine("Nhap danh sach chuoi:");
+        string[] arr = Console.ReadLine().Split(' ');
+
+        foreach (string s in arr)
+        {
+            ds.Add(s);
+        }
+
+        Console.WriteLine("Nhap chuoi can kiem tra:");
+        string kt = Console.ReadLine();
+
+        if (ds.Contains(kt))
+        {
+            Console.WriteLine("Chuoi ton tai trong danh sach");
+        }
+        else
+        {
+            Console.WriteLine("Chuoi khong ton tai");
+        }
+    }
+}
